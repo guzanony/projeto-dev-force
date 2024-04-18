99ad1d81497b46d966d23f6162202b70323ef79d
Richardson Maia <jr_maia@outlook.com>
2024-04-18 18:16:11 -0300

    Validar CPF Cliente

--- JS/page-cadastro-cliente.js
+++ JS/page-cadastro-cliente.js
@@ -4,6 +4,57 @@ document.addEventListener('DOMContentLoaded', function() {
     const divEnderecosEntrega = document.getElementById('enderecosEntrega');
     let contadorEnderecos = 0;
 
+
+    function validarCPF(cpf) {
+        cpf = cpf.replace(/[^\d]+/g, ''); // Remove tudo o que não é dígito
+        if (cpf.length !== 11) {
+            return false;
+        }
+
+        var numeros, digitos, soma, i, resultado, digitos_iguais;
+        digitos_iguais = 1;
+
+        for (i = 0; i < cpf.length - 1; i++) {
+            if (cpf.charAt(i) !== cpf.charAt(i + 1)) {
+                digitos_iguais = 0;
+                break;
+            }
+        }
+
+        if (!digitos_iguais) {
+            numeros = cpf.substring(0, 9);
+            digitos = cpf.substring(9);
+            soma = 0;
+
+            for (i = 10; i > 1; i--) {
+                soma += numeros.charAt(10 - i) * i;
+            }
+
+            resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+            if (resultado !== Number(digitos.charAt(0))) {
+                return false;
+            }
+
+            numeros = cpf.substring(0, 10);
+            soma = 0;
+
+            for (i = 11; i > 1; i--) {
+                soma += numeros.charAt(11 - i) * i;
+            }
+
+            resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+            if (resultado !== Number(digitos.charAt(1))) {
+                return false;
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     botaoAdicionarEndereco.addEventListener('click', function() {
         const novoEndereco = document.createElement('div');
         novoEndereco.innerHTML = `
