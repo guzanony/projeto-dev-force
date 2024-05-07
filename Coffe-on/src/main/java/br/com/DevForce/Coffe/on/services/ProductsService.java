b0511cf5b37502453d80ac0a8b4f39755513d834
Richardson Maia <jr_maia@outlook.com>
2024-05-07 01:08:02 -0300

    Configuração BackEnd + JAVA

--- Coffe-on/src/main/java/br/com/DevForce/Coffe/on/services/ProductsService.java
+++ Coffe-on/src/main/java/br/com/DevForce/Coffe/on/services/ProductsService.java
@@ -26,4 +26,24 @@ public class ProductsService {
         Product newProduct = new Product(requestProducts);
         return productsRepository.save(newProduct);
     }
+
+
+
+    public boolean activateUser(String id) {
+        return productsRepository.findById(id)
+                .map(product -> {
+                    product.setActive(true);
+                    productsRepository.save(product);
+                    return true;
+                }).orElse(false);
+    }
+
+    public boolean deactivateUser(String id) {
+        return productsRepository.findById(id)
+                .map(product -> {
+                    product.setActive(false);
+                    productsRepository.save(product);
+                    return true;
+                }).orElse(false);
+    }
 }
