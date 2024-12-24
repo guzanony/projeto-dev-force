cee9a3c5cc777fc45aa7270562a6ce45e43bc3ab
Gustavo <guzanony1234@gmail.com>
2024-12-23 21:23:49 -0300

    Adicionando anotação a classe principal

--- Coffe-on/src/main/java/br/com/KiloByte/KiloByteApplication.java
+++ Coffe-on/src/main/java/br/com/KiloByte/KiloByteApplication.java
@@ -2,8 +2,10 @@ package br.com.KiloByte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "br.com.KiloByte.domain.user.userAdmin")
 public class KiloByteApplication {
 
 	public static void main(String[] args) {
