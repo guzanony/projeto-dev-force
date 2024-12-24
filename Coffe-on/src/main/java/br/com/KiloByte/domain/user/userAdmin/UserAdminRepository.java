0d132f49e76ded3184f61f09e5db460d63bffeae
Gustavo <guzanony1234@gmail.com>
2024-12-23 21:14:40 -0300

    Adicionando @repository ao UserAdmin

--- Coffe-on/src/main/java/br/com/KiloByte/domain/user/userAdmin/UserAdminRepository.java
+++ Coffe-on/src/main/java/br/com/KiloByte/domain/user/userAdmin/UserAdminRepository.java
@@ -1,10 +1,11 @@
 package br.com.KiloByte.domain.user.userAdmin;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserAdminRepository extends JpaRepository<UserAdmin, String> {//Verifica o usuário pelo login
     Optional<UserAdmin> findByUsername(String username);
 
